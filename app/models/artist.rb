5dfbc57677ce6a096a17db6ff5651dbbfab5aa0e
jnf <jeremy.flores@gmail.com>
2016-04-28 08:49:11 -0700

    add display_name to make pretty names to the artist model.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,10 @@
 class Artist < ActiveRecord::Base
   has_many :albums
+
+  def display_name
+    # converts 'The Clash' to 'Clash, The'
+    return name unless name[0,4] == 'The '
+
+    name.gsub(/^The\s+/, '') + ', The'
+  end
 end

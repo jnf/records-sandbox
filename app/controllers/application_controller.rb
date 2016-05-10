7441f043599e15efc2fc8036411227083bfad08b
jnf <jeremy.flores@gmail.com>
2016-05-10 08:51:36 -0700

    add an example of how to control access with session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
+  before_action :require_login
+
   def current_user
     @user ||= User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    if current_user.nil?
+      flash[:error] = "You must be logged in to view this section"
+      redirect_to login_path
+    end
   end
 end

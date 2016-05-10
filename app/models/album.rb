e6f4c9a6076d4a63b6cf7c442f81d9a653857cef
jnf <jeremy.flores@gmail.com>
2016-05-10 10:56:34 -0700

    add example of custom validation method

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,5 +2,15 @@ class Album < ActiveRecord::Base
   validates :title, presence: true
   validates :label_code, presence: true
 
+  validate :released_cant_be_in_the_future
+
   belongs_to :artist
+
+  private
+
+  def released_cant_be_in_the_future
+    if released.present? && released > Date.today.year
+      errors.add(:released, "can't be in the future")
+    end
+  end
 end
